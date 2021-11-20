d9433d387e93ed9a56a01c2a75048dd126431499
Swiichy <adlpromail@gmail.com>
2021-11-20 09:53:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,122 @@
+const date_picker_element = document.querySelector(".date-picker");
+const selected_date_element = document.querySelector(
+  ".date-picker .selected-date"
+);
+const dates_element = document.querySelector(".date-picker .dates");
+const mth_element = document.querySelector(".date-picker .dates .month .mth");
+const next_mth_element = document.querySelector(
+  ".date-picker .dates .month .next-mth"
+);
+const prev_mth_element = document.querySelector(
+  ".date-picker .dates .month .prev-mth"
+);
+const days_element = document.querySelector(".date-picker .dates .days");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+let selectedDate = date;
+let selectedDay = day;
+let selectedMonth = month;
+let selectedYear = year;
+
+mth_element.textContent = months[month] + " " + year;
+selected_date_element.textContent = formatDate(date);
+populateDates();
+
+// EVENT LISTENERS
+date_picker_element.addEventListener("click", toggleDatePicker);
+next_mth_element.addEventListener("click", goToNextMonth);
+prev_mth_element.addEventListener("click", goToPrevMonth);
+// FUNCTION
+function toggleDatePicker(e) {
+  if (
+    !checkEventPathForClass(
+      e.path || (e.composedPath && e.composedPath()),
+      "dates"
+    )
+  ) {
+    dates_element.classList.toggle("active");
+  }
+}
+
+function goToNextMonth(e) {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  mth_element.textContent = mth_element.textContent =
+    months[month] + " " + year;
+  populateDates();
+}
+
+function goToPrevMonth(e) {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  mth_element.textContent = mth_element.textContent =
+    months[month] + " " + year;
+  populateDates();
+}
+
+function populateDates(e) {
+  days_element.innerHTML = "";
+  let amount_days = 31;
+
+  if (month == 1) {
+    amount_days = 28;
+  }
+
+  for (let i = 0; i < amount_days; i++) {
+    const day_element = document.createElement("div");
+    day_element.classList.add("day");
+    day_element.textContent = i + 1;
+
+    days_element.appendChild(day_element);
+  }
+}
+
+// HELPER
+function checkEventPathForClass(path, selector) {
+  for (let i = 0; i < path.length; i++) {
+    if (path[i].classList && path[i].classList.contains(selector)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function formatDate(d) {
+  let day = d.getDate();
+  if (day < 10) {
+    day = "0" + day;
+  }
+  let month = d.getMonth() + 1;
+  if (month < 10) {
+    day = "0" + month;
+  }
+  let year = d.getFullYear();
+  console.log(month);
+
+  return day + " / " + month + " / " + year;
+}
